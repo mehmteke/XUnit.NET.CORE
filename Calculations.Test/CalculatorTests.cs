03078be9250e42bd4bceefa70175fab77f58213e
Mehmet Teker <teker.mehmed@gmail.com>
2020-04-11 15:16:02 +0300

    Use number,double,strAsserts

---
+++ Calculations.Test/CalculatorTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculations.Test
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_GivenTwoIntValues_ReturnInt()
+        {
+            int result = Calcolator.Add(1,2);
+            Assert.Equal(3, result);
+        }
+        [Fact]
+        public void AddDouble_GivenTwoDoubleValues_ReturnDouble()
+        {
+            double result = Calcolator.AddDouble(1.2,3.5);
+            Assert.Equal(4.7,result);
+        }
+        [Fact]
+        public void MakeFullName_GivenNameAndSurname_ReturnFullName()
+        {
+            var calc = new Calcolator();
+            string result = calc.MakeFullName("Mehmet","Teker");
+            //Assert.Equal("Mehmet Teker",result, ignoreCase:true);
+            Assert.Contains("Mehmet", result,StringComparison.InvariantCultureIgnoreCase);
+            //Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", result); 
+            /* 
+               Regex bir string ifadenin (metin) belirli kurallara uyumluluğunu kontrol etmeye ve düzenlemeye yarar.
+               Açılımı Regular Expressions, Türkçesi ise Düzenli İfadeler olarak geçer.
+            */
+        }
+        [Fact]
+        public void NickName_MustBeNull()
+        {
+            var calc = new Calcolator();
+            calc.NickName = "Strong Man";
+            Assert.NotNull(calc.NickName);
+        }
+        [Fact]
+        public void MakeFullName_AlwaysReturnValues()
+        {
+            var calc = new Calcolator();
+            string result = calc.MakeFullName("Mehmet","Teker");
+            Assert.True(!string.IsNullOrEmpty(result));
+        }
+
+    }
+}
--- Calculations/Calcolator.cs
+++ Calculations/Calcolator.cs
@@ -6,8 +6,19 @@ namespace Calculations
 {
     public class Calcolator
     {
+        public string NickName { get; set; }
         public static int Add(int a, int b) {
             return a + b;
+        }
+
+        public static double AddDouble(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public string MakeFullName(string name, string surname)
+        {
+            return $"{name}-{surname}";
         }
     }
 }
