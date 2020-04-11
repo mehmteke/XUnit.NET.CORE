da718b0ce3c3c90b67c0ef2fd2739c8d128a5eea
Mehmet Teker <teker.mehmed@gmail.com>
2020-04-11 12:44:43 +0300

    First XUnit Test and Phases of Unit Testing

---
+++ Calculations.Test/CalculatorTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculations.Test
+{
+    
+    public class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.True(true);  
+        }
+    }
+}
---
+++ Calculations/Calcolator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculations
+{
+    public class Calcolator
+    {
+        public int Add(int a, int b) {
+            return a + b;
+        }
+    }
+}
