740db8e8c24bd35aa4e7243510008e75b94e1dc0
Mehmet Teker <teker.mehmed@gmail.com>
2020-04-11 18:28:58 +0300

    Asserts Exceptions and Object Type

---
+++ Calculations.Test/CustomerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculations.Test
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void GetOrdersByNameNotNull()
+        {
+            var customer = new Customer();
+            var error = Assert.Throws<ArgumentException>(()=>customer.GetOrdersByName(""));
+            Assert.Equal("Hata",error.Message.ToString());
+        }
+
+        [Fact]
+        public void CreateCustomerInstance_GivenOrderCount_ReturnObject()
+        {
+            var calc = CustomerFactory.CreateCustomerInstance(100);
+            Assert.IsNotType<LoyalCustomer>(calc);
+        }
+         
+    }
+}
---
+++ Calculations/Customer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculations
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual int GetOrdersByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new AggregateException("Hata");
+            }
+            return 100;
+        }
+    }
+
+    public class LoyalCustomer: Customer {
+        public int Discount { get; set; }
+
+        public LoyalCustomer()
+        {
+            Discount = 10;
+        }
+
+        public override int GetOrdersByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new AggregateException("Hata");
+            }
+            return 100;
+        }
+    }
+
+    public static class CustomerFactory
+    {
+        public static Customer CreateCustomerInstance(int orderCount)
+        {
+            if(orderCount > 10)
+            {
+                return new Customer();
+            }
+            else
+            {
+                return new LoyalCustomer();
+            }
+        }
+
+    }
+}
